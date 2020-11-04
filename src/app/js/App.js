bda2787518d9c3b3db8885d4e656f43e6a59ed4b
jjjj <cmbox123@yandex.by>
2020-11-04 18:00:57 +0300

    fix serch input

--- src/app/js/App.js
+++ src/app/js/App.js
@@ -2,11 +2,30 @@ window.onload = function () {
     const navMenu = document.querySelector('.nav__menu');
     const burgerBtn = document.querySelector('.burger__btn');
     const body = document.querySelector('body');
+    const overlay = document.querySelector('.overlay');
 
-    burgerBtn.addEventListener('click', function (evt) {
+        burgerBtn.addEventListener('click', function (evt) {
         evt.preventDefault();
         burgerBtn.classList.toggle('burger__btn--active')
         navMenu.classList.toggle('nav__menu--active');
         body.classList.toggle('hidden');
+        overlay.classList.toggle('overlay--active');
+    })
+
+    overlay.addEventListener('click', function(){
+        burgerBtn.classList.remove('burger__btn--active')
+        navMenu.classList.remove('nav__menu--active');
+        body.classList.remove('hidden');
+        overlay.classList.remove('overlay--active');
+    })
+
+
+    let inputSearch = document.querySelector('input[type="search"]');
+    let inputClearSearch = document.querySelector('.search__clear');
+
+    inputClearSearch.addEventListener('click', function (e) {
+        e.preventDefault();
+        inputSearch.value = '';
     })
 }
+
