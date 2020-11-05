757d27c9e281c8860aa7e120b94fa5fd80480080
jjjj <cmbox123@yandex.by>
2020-11-05 17:56:05 +0300

    markup footer

--- src/app/js/App.js
+++ src/app/js/App.js
@@ -67,6 +67,23 @@ window.onload = function () {
     //         }
     //     });
     // }
+    let btn = document.querySelectorAll('a[href^="tel:"]');
+
+    let resizeWindow = function (evt) {
+        if (window.innerWidth <= 1100) {
+            evt.stopPropagation();
+        } else {
+            evt.preventDefault();
+        }
+    };
+
+    btn.forEach(function (item) {
+        item.addEventListener('click', resizeWindow);
+    });
+
+    btn.forEach(function (item) {
+        item.addEventListener('resize', resizeWindow);
+    });
 
 }
 
