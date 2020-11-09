7b1950d9a70b1796b9014044a7a8b4afd25a53bc
jjjj <cmbox123@yandex.by>
2020-11-09 14:22:30 +0300

    finished markup populat slider

--- src/app/js/swiper-initialization.js
+++ src/app/js/swiper-initialization.js
@@ -7,19 +7,45 @@ var swiper = new Swiper('.swiper-main', {
         el: '.swiper-pagination',
         clickable: true,
     },
-    loop:true,
-    loopPreventsSlide:false,
+    loop: true,
+    loopPreventsSlide: false,
 
 });
 
 var swiper = new Swiper('.swiper-popular', {
-    slidesPerView: 5,
-    spaceBetween: 30,
+    slidesPerView: 4,
+    spaceBetween: 15,
+    loop: true,
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
-    loopPreventsSlide:false,
+    loopPreventsSlide: false,
+
+
+    breakpoints: {
+        320: {
+            slidesPerView: "auto",
+            centeredSlides: true,
+        },
+        1024: {
+            centerSlides: false,
+            slidesPerView: 4,
+            spaceBetween: 10,
+        },
+        1025: {
+            slidesPerView: 3,
+        },
+        1098: {
+            slidesPerView: 4,
+        },
+        // when window width is >= 320px
+        1454: {
+            slidesPerView: 5,
+
+        },
+
+    }
 
 });
 
